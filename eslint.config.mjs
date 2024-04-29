da8c30b3496c7e8b7322732564108319ef650774
Pablo Maffioli <92764736+pablo-maff@users.noreply.github.com>
2024-04-29 20:14:19 +0000

    - new eslint version and config file - removed mongo.js file

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    ignores: ['dist', 'node_modules'],
+
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+      '@stylistic/js/no-trailing-spaces': 'error',
+      '@stylistic/js/arrow-spacing': 'error',
+      '@stylistic/js/object-curly-spacing': ['error', 'always'],
+      'no-console': 0,
+      'eqeqeq': 'error'
+    }
+  }
+]
